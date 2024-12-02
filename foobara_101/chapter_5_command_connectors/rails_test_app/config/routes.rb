c3844a7835331c13496fcdc4c6857938c12a963a
Miles Georgi <azimux@gmail.com>
2024-12-02 15:56:36 -0800

    Successfully wire up commands with rails router

--- foobara_101/chapter_5_command_connectors/rails_test_app/config/routes.rb
+++ foobara_101/chapter_5_command_connectors/rails_test_app/config/routes.rb
@@ -1,4 +1,19 @@
+require_relative "../../part_3_rails_connector"
+require "foobara/rails_command_connector"
+
+Foobara::CommandConnectors::RailsCommandConnector.new
+
+# command_connector.connect(CreateCapybara)
+# command_connector.connect(IncrementAge)
+# command_connector.connect(FindCapybara)
+
+require "foobara/rails/routes"
+
 Rails.application.routes.draw do
+  command CreateCapybara
+  command IncrementAge
+  command FindCapybara
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
