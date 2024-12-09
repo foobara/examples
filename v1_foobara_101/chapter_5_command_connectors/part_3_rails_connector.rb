3e67159f0a35f55b25cc0a7e3c2fbe45071a9f6f
Miles Georgi <azimux@gmail.com>
2024-12-09 15:08:13 -0800

    Get rails connector example working

--- v1_foobara_101/chapter_5_command_connectors/part_3_rails_connector.rb
+++ v1_foobara_101/chapter_5_command_connectors/part_3_rails_connector.rb
@@ -1,11 +1,79 @@
-# this goes in routes.rb
+require "foobara"
+require "foobara/local_files_crud_driver"
 
-# rails new --api --skip-docker --skip-asset-pipeline --skip-javascript --skip-hotwire --skip-jbuilder --skip-test --skip-brakeman --skip-kamal --skip-solid rails_test_app
+# see routes.rb in rails_test_app/
 
-require "foobara/rails_command_connector"
+crud_driver = Foobara::LocalFilesCrudDriver.new
+Foobara::Persistence.default_crud_driver = crud_driver
 
-Foobara::CommandConnectors::RailsCommandConnector.new
+class Capybara < Foobara::Entity
+  attributes do
+    id :integer
+    name :string, :required, "Official name"
+    nickname :string, "Informal name for friends"
+    age :integer, :required, "The number of times this capybara has gone around the sun"
+  end
 
-command_connector.connect(CreateCapybara)
-command_connector.connect(IncrementAge)
-command_connector.connect(FindCapybara)
+  primary_key :id
+end
+
+class CreateCapybara < Foobara::Command
+  description "Just creates a capybara!"
+
+  inputs Capybara.attributes_type
+  result Capybara
+
+  def execute
+    create_capybara
+
+    capybara
+  end
+
+  attr_accessor :capybara
+
+  def create_capybara
+    self.capybara = Capybara.create(inputs)
+  end
+end
+
+class FindCapybara < Foobara::Command
+  description "Just tell us who you want to find!"
+
+  inputs do
+    id Capybara.primary_key_type, :required
+  end
+
+  result Capybara
+
+  def execute
+    load_capybara
+
+    capybara
+  end
+
+  attr_accessor :capybara
+
+  def load_capybara
+    self.capybara = Capybara.load(id)
+  end
+end
+
+class IncrementAge < Foobara::Command
+  description "A trip around the sun!"
+
+  inputs do
+    capybara Capybara, :required
+  end
+
+  result Capybara
+
+  def execute
+    increment_age
+
+    capybara
+  end
+
+  def increment_age
+    capybara.age += 1
+  end
+end
--- v1_foobara_101/chapter_5_command_connectors/rails_test_app/config/routes.rb
+++ v1_foobara_101/chapter_5_command_connectors/rails_test_app/config/routes.rb
@@ -1,18 +1,18 @@
 require_relative "../../part_3_rails_connector"
 require "foobara/rails_command_connector"
 
-Foobara::CommandConnectors::RailsCommandConnector.new
+command_connector = Foobara::CommandConnectors::RailsCommandConnector.new
 
-# command_connector.connect(CreateCapybara)
-# command_connector.connect(IncrementAge)
-# command_connector.connect(FindCapybara)
+command_connector.connect(CreateCapybara)
+command_connector.connect(IncrementAge)
+command_connector.connect(FindCapybara)
 
 require "foobara/rails/routes"
 
 Rails.application.routes.draw do
-  command CreateCapybara
-  command IncrementAge
-  command FindCapybara
+  # command CreateCapybara
+  # command IncrementAge
+  # command FindCapybara
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
